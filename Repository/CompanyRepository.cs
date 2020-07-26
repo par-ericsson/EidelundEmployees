5ff6ceaa8946e1d3336d440579e3f7c095411110
Pär Ericsson <ericsson@home.se>
2020-07-26 19:38:47 +0200

    Added CompaniesController and AutoMapper

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -1,6 +1,8 @@
 ﻿using Contracts;
 using Entities;
 using Entities.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Repository
 {
@@ -10,5 +12,10 @@ namespace Repository
             :base(repositoryContext)
         {
         }
-    }
+
+        public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
+            FindAll(trackChanges)
+            .OrderBy(c => c.Name)
+            .ToList();
+        }
 }
