910cc0aeafaf06917e5a1b048332decaf8208d46
Pär Ericsson <ericsson@home.se>
2020-07-28 19:47:21 +0200

    Implemented a custom formatter

--- EidelundEmployees/Extensions/ServiceExtensions.cs
+++ EidelundEmployees/Extensions/ServiceExtensions.cs
@@ -29,5 +29,8 @@ namespace EidelundEmployees.Extensions
 
         public static void ConfigureRepositoryManager(this IServiceCollection services) =>
             services.AddScoped<IRepositoryManager, RepositoryManager>();
+
+        public static IMvcBuilder AddCustomCSVFormatter(this IMvcBuilder builder) =>
+            builder.AddMvcOptions(config => config.OutputFormatters.Add(new CsvOutputFormatter()));
     }
 }
