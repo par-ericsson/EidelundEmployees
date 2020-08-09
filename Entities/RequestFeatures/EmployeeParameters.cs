ce8ace7f11fb8dc96a98494d569d5bb3d92d1a70
Pär Ericsson <ericsson@home.se>
2020-08-09 10:18:15 +0200

    Added search functionality

--- Entities/RequestFeatures/EmployeeParameters.cs
+++ Entities/RequestFeatures/EmployeeParameters.cs
@@ -7,5 +7,6 @@
 
         public bool ValidAgeRange => MaxAge > MinAge;
 
+        public string SearchTerm { get; set; }
     }
 }
--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Repository.Extensions;
 
 namespace Repository
 {
@@ -19,12 +20,14 @@ namespace Repository
 
         public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
         {
-            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId) && (e.Age >= employeeParameters.MinAge && e.Age <= employeeParameters.MaxAge), trackChanges)
-              .OrderBy(e => e.Name)
-              .ToListAsync();
+            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
+                .FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)
+                .Search(employeeParameters.SearchTerm)
+                .OrderBy(e => e.Name)
+                .ToListAsync();
 
             return PagedList<Employee>
-              .ToPagedList(employees, employeeParameters.PageNumber, employeeParameters.PageSize);
+                .ToPagedList(employees, employeeParameters.PageNumber, employeeParameters.PageSize);
         }
 
         public async Task<Employee> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges) =>
